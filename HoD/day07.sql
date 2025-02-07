f46aee169ebba6e5fce9eb90765f31e91f8172e8
dajonss <davidjonsson061@gmail.com>
2025-02-07 09:34:59 +0100

    Solution for day07

--- HoD/day07.sql
+++ HoD/day07.sql
@@ -3,15 +3,16 @@
 --Same item different color
 --Same time
 
-select
-  orders.shipped,
-  sku,
-  products.desc
-  from orders
-       join orders_items using (orderid)
-       join products using (sku)
- where customerid = 4167
-   and products.desc like '%(%)%'
-       ;
+SELECT shipped, sku, desc, name, phone
+	FROM customers
+	JOIN orders USING (customerid)
+	JOIN orders_items USING (orderid)
+	JOIN products using (SKU)
+	WHERE (shipped LIKE '2018-12-31 12:26%' 
+		OR shipped LIKE '2020-06-28 11:36%' 
+		OR shipped LIKE '2021-10-07 20:15%' 
+		OR shipped LIKE '2022-04-23 18:45%')
+		AND products.desc like '%(%)%'
+	ORDER BY name;
 
 
